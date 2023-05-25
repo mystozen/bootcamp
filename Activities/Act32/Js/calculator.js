0d5a93fef26061605a224ddfc7218263fef4dbf3
mystozen <mystozen@gmail.com>
2023-05-26 06:39:04 +0800

    Add/Change/Rearrange File

---
+++ Activities/Act32/Js/calculator.js
@@ -0,0 +1,9 @@
+function runactivity() {
+  let x = Number(prompt("What is your first number of your age?"));
+  let y = Number(
+    prompt("What is the last 2 diget number of your mobile phone?")
+  );
+  z = x + y;
+  console.log(x + y);
+  alert(z);
+}
