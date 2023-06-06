1c1ad6148e6757a726306ba64275304930348478
mystozen <mystozen@gmail.com>
2023-06-06 09:51:50 +0800

    adding new activiry

---
+++ Activities/Act44/js/change_name.js
@@ -0,0 +1,5 @@
+function changeName() {
+  let name = prompt("What is your name");
+  document.getElementById("userName").innerHTML = name;
+  document.getElementById("userName2").innerHTML = name;
+}
